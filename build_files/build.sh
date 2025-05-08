c22d49bc723eb7da1192c00d2eb3dd521b7b70ff
Jeremy Graeme <jeremy.graeme@hey.com>
2025-05-08 12:25:33 -0700

    Arandr

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 dnf5 install -y emacs
-dnf5 install -y @base-x gcc libXft-devel libX11-devel libXinerama-devel arandr xorg-x11-xinit-session
+dnf5 install -y @base-x gcc libXft-devel libX11-devel libXinerama-devel arandr xorg-x11-xinit-session arandr
 
 # Use a COPR Example:
 #
